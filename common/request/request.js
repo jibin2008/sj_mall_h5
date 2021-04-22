41433cd920d587a8090b349a91b552e517c7fc89
季彬 <jibin2008@126.com>
2021-04-22 16:59:16 +0800

    临时提交

--- common/request/request.js
+++ common/request/request.js
@@ -57,16 +57,18 @@ const resInterceptor = (response, conf={}) => {
     // 必须返回你需要处理的数据，将会进入resolve（then中处理）
     // 如果需要reject，需要设置mypReqToReject:true，还可以携带自己定义的任何提示内容（catch中处理）
 	if(conf.header.encrypt === "aes"){
-		debugger
-		let data = JSON.parse(Decrypt(response.data.content))
-		if(data.seq === conf.header.encryptSeq){
-			response.data = data.data
-		}else{
-			return {
-				mypReqToReject:true,
-				errorMsg:"数据异常！！！"
+		if(response.statusCode === 200){
+			let data = JSON.parse(Decrypt(response.data.content))
+			if(data.seq === conf.header.encryptSeq){
+				response.data = data.data
+			}else{
+				return {
+					mypReqToReject:true,
+					errorMsg:"数据异常！！！"
+				}
 			}
-		}
+		}else
+			return response
 	}
     return response
 }
