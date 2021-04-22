86e6dbc41e12859aeb527a52499bd54665c97cfb
季彬 <jibin2008@126.com>
2021-04-22 10:58:50 +0800

    遗漏

---
+++ pages/index/api.js
@@ -0,0 +1,18 @@
+import Request from "../../common/request/request.js"
+
+function req(url,data){
+	return Request.request({
+		url,
+		method: 'POST',
+		data,
+		header:{
+			encrypt:"aes"
+		}
+	})
+}
+
+export default{
+	isTarget(phone){
+		return req('/gzwz/service/dczr/isTarget',{phone})
+	}
+}
